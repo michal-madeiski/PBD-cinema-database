c63ff79bdab8f32a9ef65faddc0d1027c1d55d40
Michal Madeiski <michal.madeiski@onet.pl>
2025-11-05 12:41:01 +0100

    Fix small bugs

--- seeder.py
+++ seeder.py
@@ -55,15 +55,21 @@ def seed_special_offer(n):
     start_date_end = start_date_start+timedelta(days=900)
 
     seeder((SpecialOffer(name = faker.word(),
-                         start_time = faker.date_time_between(start_date=start_date_start, end_date=start_date_end),
-                         end_time = faker.date_time_between(start_date=start_date_end+timedelta(days=1), end_date=datetime.now()+timedelta(days=30)),
+                         start_time = faker.date_time_between(start_date=start_date_start, end_date=start_date_end).replace(second=0, microsecond=0),
+                         end_time = None if random.random() < 0.1 else faker.date_time_between(start_date=start_date_end+timedelta(days=1), end_date=datetime.now()+timedelta(days=30)).replace(second=0, microsecond=0),
                          amount = faker.random_int(min=1, max=10)
                          )
                          for _ in range(n)), "special_offer", n)
 
 def seed_seat(n):
     session = SessionLocal()
-    room_ids = [x.id for x in session.query(Room._id).all()]
+    room_ids = [x[0] for x in session.query(Room._id).all()]
+
+    if len(room_ids) == 0 :
+        print("Brak id w room")
+        session.close()
+        return
+    
     seeder((Seat(fk_room_id = random.choice(room_ids),
                  seat_num = i
                  )
@@ -72,19 +78,44 @@ def seed_seat(n):
 
 def seed_ticket(n):
     session = SessionLocal()
-    ticket_type_ids = [x.id for x in session.query(TicketType._id).all()]
-    seat_ids = [x.id for x in session.query(Seat._id).all()]
-    screening_ids = [x.id for x in session.query(Screening._id).all()]
-    discount_ids = [x.id for x in session.query(Discount._id).all()]
-    payment_ids = [x.id for x in session.query(Payment._id).all()]
+    ticket_type_ids = [x[0] for x in session.query(TicketType._id).all()]
+    seat_ids = [x[0] for x in session.query(Seat._id).all()]
+    screening_ids = [x[0] for x in session.query(Screening._id).all()]
+    discount_ids = [x[0] for x in session.query(Discount._id).all()]
+    payment_ids = [x[0] for x in session.query(Payment._id).all()]
 
     statuses = ['used', 'valid', 'reserved', 'payment_pending', 'free']
+
+    if len(ticket_type_ids) == 0 :
+        print("Brak id w ticket_type")
+        session.close()
+        return
+    
+    if len(seat_ids) == 0 :
+        print("Brak id w seat")
+        session.close()
+        return
+    
+    if len(screening_ids) == 0 :
+        print("Brak id w screening")
+        session.close()
+        return
+    
+    if len(discount_ids) == 0 :
+        print("Brak id w discount")
+        session.close()
+        return
+    
+    if len(payment_ids) == 0 :
+        print("Brak id w payment")
+        session.close()
+        return
 
     seeder((Ticket(fk_ticket_type_id = random.choice(ticket_type_ids),
                    fk_seat_id = random.choice(seat_ids),
                    fk_screening_id = random.choice(screening_ids),
-                   fk_discount_id = random.choice(discount_ids),
-                   fk_payment_id = random.choice(payment_ids),
+                   fk_discount_id = None if random.random() < 0.5 else random.choice(discount_ids),
+                   fk_payment_id = None if random.random() < 0.25 else random.choice(payment_ids),
                    qr_code = faker.word(),
                    status = random.choice(statuses),
                    price = 0 # TO-DO: pętla uzupełniająca to realnie (tak samo amount w payment)
@@ -96,8 +127,18 @@ def seed_ticket(n):
 # TABELA POSREDNIA - NIE MODEL ORM - TROCHE INACZEJ
 def seed_ticket_special_offer(n):
     session = SessionLocal()
-    ticket_ids = [x.id for x in session.query(Ticket._id).all()]
-    special_offer_ids = [x.id for x in session.query(SpecialOffer._id).all()]
+    ticket_ids = [x[0] for x in session.query(Ticket._id).all()]
+    special_offer_ids = [x[0] for x in session.query(SpecialOffer._id).all()]
+
+    if len(ticket_ids) == 0 :
+        print("Brak id w ticket")
+        session.close()
+        return
+    
+    if len(special_offer_ids) == 0 :
+        print("Brak id w special_offer")
+        session.close()
+        return
 
     # set bo para jako klucz główny czyli ma być bez powtórzeń
     pairs = set()
@@ -115,6 +156,36 @@ def seed_ticket_special_offer(n):
     except Exception as e:
         session.rollback()
         print("Błąd podczas seedowania:", e)
+    finally: 
+        session.close()
+
+
+def seed_ticket_with_realistic_price():
+    session = SessionLocal()
+    tickets = session.query(Ticket).all()
+
+    try: 
+        for t in tickets:
+            discount_obj = session.query(Discount).filter(Discount._id == t.fk_discount_id).first()
+            dsc = discount_obj.percentage if discount_obj else None
+            type_price = session.query(TicketType).filter(TicketType._id == t.fk_ticket_type_id).first().price
+            special_offers = [s.amount for s in t.fk_special_offer]
+
+            new_price_base = type_price
+
+            for i in range(len(special_offers)):
+                if new_price_base > 0:
+                    new_price_base -= special_offers[i]
+
+            if dsc is not None:
+                new_price = new_price_base - (dsc/100)*new_price_base
+
+            t.price = new_price
+        session.commit()
+        print("Zmieniono ceny biletów na realistyczne")
+    except Exception as e:
+        session.rollback()
+        print("Błąd podczas zmiany cen biletów:", e)
     finally: 
         session.close()
 
