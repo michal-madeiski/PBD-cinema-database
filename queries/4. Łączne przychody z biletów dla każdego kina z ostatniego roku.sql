e9a4a11be74d6cd0d2373093c4588fcd71f68b5b
Mikołaj Kamiński <96744056+Drake3001@users.noreply.github.com>
2025-11-12 13:56:25 +0100

    queries 1-5

---
+++ queries/4. Łączne przychody z biletów dla każdego kina z ostatniego roku.sql
@@ -0,0 +1,8 @@
+SELECT c._id, SUM(t.price) AS total_ticket_income
+FROM cinema c FULL 
+JOIN room r ON r.fk_cinema_id=c._id
+JOIN screening s ON s.fk_room_id=r._id
+JOIN ticket t ON t.fk_screening_id=s._id
+WHERE s.start_time >= (CURRENT_DATE - INTERVAL '1 year')
+GROUP BY c._id
+ORDER BY total_ticket_income DESC; 
