7c1f13d198b935a9d05005f9d4b65cb3f6db0182
Michal Madeiski <michal.madeiski@onet.pl>
2025-11-14 17:54:55 +0100

    Add param query

---
+++ sample_queries/q_21.sql
@@ -0,0 +1,25 @@
+--Which seats are available for the given screening?
+PREPARE q (int) AS
+SELECT
+    s.seat_num as "seat number"
+FROM
+    seat s
+JOIN
+    room r
+    ON s.fk_room_id = r._id
+JOIN
+    screening sc
+    ON sc.fk_room_id = r._id
+JOIN
+    ticket t
+    ON t.fk_screening_id = sc._id
+WHERE
+    t.status = 'free'
+    AND
+    sc._id = $1
+GROUP BY
+    s._id
+ORDER BY
+    s.seat_num ASC;
+EXECUTE q(17);
+EXECUTE q(61);
